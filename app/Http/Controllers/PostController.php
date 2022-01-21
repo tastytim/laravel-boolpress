a8cbbfc63f1eb85ee5a674d05c9f623a2aaf3e5f
tastytim <tastytimgm@gmail.com>
2022-01-21 15:03:17 +0100

    pivot post_tag created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,7 @@ class PostController extends Controller
 
     // ];
 
-    $postsPublicList = Post::with("category", "user")->get();
+    $postsPublicList = Post::with("category", "user","tags")->get();
 
     foreach($postsPublicList as $post){
       $post['body']= strlen($post->body) > 190 ? substr($post->body , 0 , 190) : $post->body;
--- app/Post.php
+++ app/Post.php
@@ -17,4 +17,9 @@ class Post extends Model
     public function category(){
         return $this->belongsTo('App\Category');
     }
+
+
+    public function tag(){
+        return $this->belongsToMany('App\Tag');
+    }
 }
